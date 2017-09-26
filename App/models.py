f7743d46063d78694b42e5a599ecdf918a909970
YuQibing <1174797939@qq.com>
2017-09-26 16:14:45 +0800

    add Whell model

--- App/models.py
+++ App/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Whell(models.Model):
+    img = models.CharField(max_length=200)
+    name = models.CharField(max_length=20)
+    trackid = models.CharField(max_length=20)
+    class Meta:
+        db_table = 'axf_whell'
